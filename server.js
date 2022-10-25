543a9e60abf01a9d3c38c0258307185f8cbe3517
Krishna Aher <63346973+kisna68@users.noreply.github.com>
2022-10-25 15:23:18 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const http = require("http");
+const path = require("path");
 const { Server } = require("socket.io");
 const ACTIONS = require("./src/Actions");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+
+app.use(express.static("build"));
+app.use((req, res, next) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
 const userSocketMap = {};
 
 function getAllConnectedClients(roomId) {
